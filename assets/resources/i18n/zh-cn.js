39bf108ab60104f3c5bdc44ada3febbefef9d928
Kerlin Huang <ikerlin@msn.com>
2018-04-14 11:49:18 +0800

    增加语言包

---
+++ assets/resources/i18n/zh-cn.js
@@ -0,0 +1,14 @@
+'use strict';
+
+if (!window.i18n) {
+    window.i18n = {};
+}
+
+if (!window.i18n.languages) {
+    window.i18n.languages = {};
+}
+
+window.i18n.languages['zh-cn'] = {
+    // write your key value pairs here
+    "Test Button": "测试按钮",
+};
